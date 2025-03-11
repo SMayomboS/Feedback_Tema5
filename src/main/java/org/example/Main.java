a353d6e9dee39db83bccd537c0999ebe2c062396
Sergio Mayombo Serrano <smayoser@myuax.com>
2025-03-11 20:15:03 +0100

    main para probar cada método

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
+import adapter.*;
+import factory.*;
+import observer.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Prueba de Factory Method
+        TareaFactory factory = new DesarrolloFactory();
+        Tarea tarea = factory.crearTarea();
+        tarea.ejecutar();
+
+        // Prueba de Adapter
+        ServicioExterno api = new APIextrerna();
+        AdaptadorAPI adaptador = new AdaptadorAPI(api);
+        System.out.println(adaptador.obtenerDatosAdaptados());
+
+        // Prueba de Observer
+        Notificador notificador = new Notificador();
+        Usuario usuario1 = new Usuario("Juan");
+        Usuario usuario2 = new Usuario("Ana");
+
+        notificador.agregarObservador(usuario1);
+        notificador.agregarObservador(usuario2);
+
+        notificador.notificar("Se ha creado una nueva tarea en el sistema.");
     }
 }
